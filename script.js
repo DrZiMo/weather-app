42190ed79b2ac393532b390f32b004587ce04620
DrZiMo <zuhaybhamar@gmail.com>
2024-11-18 15:12:55 +0300

    added some if clauses

--- script.js
+++ script.js
@@ -19,8 +19,19 @@ const getWheather = async () => {
 
     const API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`;
 
+    if (!city) {
+        alert("Please enter city name!");
+        return;
+    }
+
     const response = await fetch(API_URL);
     const data = await response.json();
+
+    if (data.cod == '404') {
+        alert("Please enter valid city name");
+        cityInput.value = "";
+        return;
+    }
 
     console.log(data);
     cityName.textContent = data.city.name;
@@ -37,7 +48,7 @@ const getWheather = async () => {
     sunset.textContent = `${sunsetTime.getHours()}:${sunsetTime.getMinutes() < 10 ? "0" + sunsetTime.getMinutes() : sunsetTime.getMinutes()}:${sunsetTime.getSeconds()}`;
 
     // next 4 days
-    for(let i = 5; i <= 29; i += 8) {
+    for (let i = 5; i <= 29; i += 8) {
         let dayDate = new Date(data.list[i].dt * 1000);
         let dayName = document.querySelector(`.day-${(i - 5) / 8 + 1}-name`);
         let dayImg = document.querySelector(`.weather-img-day-${(i - 5) / 8 + 1}`);
@@ -46,7 +57,7 @@ const getWheather = async () => {
         dayImg.src = `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`;
         dayTemp.textContent = (data.list[i].main.temp - 273).toFixed(1);
 
-        switch(dayDate.getDay()) {
+        switch (dayDate.getDay()) {
             case 1:
                 dayName.textContent = "Mon";
                 break;
